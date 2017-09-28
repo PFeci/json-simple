57b328a65524fffa4e4117bdafff20a94a960690
DESKTOP-2JEJU0Q\FeciP <fplutzer@gmail.com>
2017-09-28 22:26:58 +0200

    remove unused method

--- src/main/java/org/json/simple/parser/Yylex.java
+++ src/main/java/org/json/simple/parser/Yylex.java
@@ -203,18 +203,6 @@ class Yylex {
     return result;
   }
 
-  private static int zzUnpackAttribute(String packed, int offset, int [] result) {
-    int i = 0;       /* index in packed string  */
-    int j = offset;  /* index in unpacked array */
-    int l = packed.length();
-    while (i < l) {
-      int count = packed.charAt(i++);
-      int value = packed.charAt(i++);
-      do result[j++] = value; while (--count > 0);
-    }
-    return j;
-  }
-
   /** the input device */
   private java.io.Reader zzReader;
 
